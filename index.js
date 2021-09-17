d8d2f38dc2b48c9002ffb09e67adf26190345302
Luis <pereira1214@gmail.com>
2021-09-16 23:09:44 -0400

    commit tree 05856b01516a8ab50d67e7ab118016968cc67689

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+const body = document.querySelectorAll("body");
+
+main.remove();
+
+const newHeader = document.createElement('h1')
+
+console.log(newHeader);
+
+newHeader.id = ("victory")
+
+console.log(document.querySelectorAll("body"))
+
+newHeader.innerHTML = "LUIS is the champion"
+
+document.body.append(newHeader);
+
+
+
+
+
